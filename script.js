25e31731bf106dbfb458eee98a58488f06ccca9a
SAI TEJA H R <98202897+Saitejahr@users.noreply.github.com>
2023-03-22 17:47:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-function majority(){
-	let max =0, count=0;
-	for(let i=0; i<num.length; i++){
-		if(count ==0)max =num[i];//2,
-		if(nums[i]== max) count ++; //1
-		else count --;
-	}
-	//return max;
-	console.log(max);
-}
-majority();
+function majorityElementChecker(arr)
+    {
+        let map = new Map();
+ 
+        for(let i = 0; i < arr.length; i++) {
+            if (map.has(arr[i])) {
+                    let count = map.get(arr[i]) +1;
+                    if (count > arr.length /2) {
+                        document.write("Majority found :- " + arr[i]);
+                        return;
+                    } else
+                        map.set(arr[i], count);
+ 
+            }
+            else
+                map.set(arr[i],1);
+            }
+            document.write(" No Majority element");
+    }
+majorityElementChecker()
